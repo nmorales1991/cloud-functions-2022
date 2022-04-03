e9a840a3e7a4fe57f3111fbd2d78b55af5fa0256
Nicolas Morales <nicolas.morales@autofact.cl>
2022-04-03 04:35:20 -0400

    port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express')
-const { port, hola} = require('./config');
+const { hola } = require('./config');
 const app = express()
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send(`Hola Mundo ${ hola }`);
